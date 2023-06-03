fe9a63502cbd3cefc42055943ed1a4e4ae644677
walpizar <walpizar@hotmail.com>
2023-06-03 15:23:24 -0600

    modificar producto

--- src/controller/ProductosController.ts
+++ src/controller/ProductosController.ts
@@ -29,22 +29,15 @@ class ProductosController {
       }
 
       const productosRepo = AppDataSource.getRepository(Producto);
-      
+
       let producto;
       try {
         producto = await productosRepo.findOneOrFail({ where: { id } });
       } catch (error) {
         return resp
-        .status(404)
-        .json({ mensaje: "No se encontro el producto con ese ID" });
+          .status(404)
+          .json({ mensaje: "No se encontro el producto con ese ID" });
       }
-      
-
-      // if (!producto) {
-      //   return resp
-      //     .status(404)
-      //     .json({ mensaje: "No se encontro el producto con ese ID" });
-      // }
 
       return resp.status(200).json(producto);
     } catch (error) {
@@ -54,15 +47,70 @@ class ProductosController {
 
   static add = async (req: Request, resp: Response) => {
     try {
+      //DESTRUCTURING
+      const { id, nombre, precio, stock, fechaIngreso } = req.body;
 
+      //validacion de datos de entrada
+      if(!id){
+        return resp
+        .status(404)
+        .json({ mensaje: "Debe indicar el ID" });
+      }
+      if(!nombre){
+        return resp
+        .status(404)
+        .json({ mensaje: "Debe indicar el nombre del producto" });
+      }
+      if(!precio){
+        return resp
+        .status(404)
+        .json({ mensaje: "Debe indicar el precio" });
+      }
+      if(precio<0){
+        return resp
+        .status(404)
+        .json({ mensaje: "Debe indicar un precio mayor que 0" });
+      }
+      if(!stock){
+        return resp
+        .status(404)
+        .json({ mensaje: "Debe indicar el stock del producto" });
+      }
+      if(stock<0){
+        return resp
+        .status(404)
+        .json({ mensaje: "El stock debe ser mayor que ser" });
+      }
 
+      //validacion de reglas de negocio
+      const productosRepo = AppDataSource.getRepository(Producto);
+      const pro= await productosRepo.findOne({where:{id}});
+
+      if(pro){
+        return resp
+        .status(404)
+        .json({ mensaje: "El producto ya existe en la base datos." });
+      }
+
+      const fecha =new Date();
+
+      let producto= new Producto();
+      producto.id= id;
+      producto.nombre= nombre;
+      producto.precio= precio;
+      producto.stock= stock;
+      producto.fechaIngreso= fecha;
+      producto.estado=true;
+
+      await productosRepo.save(producto);
+      return resp.status(201).json({ mensaje: "Producto creado" });
 
 
     } catch (error) {
       return resp.status(400).json({ mensaje: error });
     }
 
-    return resp.status(200).json({ mensaje: "create" });
+
   };
 
   static update = async (req: Request, resp: Response) => {};
